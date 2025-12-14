ec8569f31f17082bc4380b07b5802cc409fb5bc5
DevBD1 <burakdorman@gmail.com>
2025-12-14 12:47:28 +0300

    feat: Changed the main Form.

---
+++ src/UI/MainAppForm.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace graphSNA.UI
+{
+    partial class MainAppForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainAppForm));
+            splitContainer1 = new SplitContainer();
+            panel1 = new Panel();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            groupBox1 = new GroupBox();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            groupBox2 = new GroupBox();
+            tabPage2 = new TabPage();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            flowLayoutPanel1.SuspendLayout();
+            groupBox1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(panel1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(tabControl1);
+            splitContainer1.Size = new Size(1064, 681);
+            splitContainer1.SplitterDistance = 800;
+            splitContainer1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 681);
+            panel1.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(260, 681);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.BackColor = SystemColors.ActiveCaption;
+            tabPage1.Controls.Add(flowLayoutPanel1);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(252, 653);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Functions";
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.BackColor = SystemColors.ActiveCaption;
+            flowLayoutPanel1.Controls.Add(groupBox1);
+            flowLayoutPanel1.Controls.Add(groupBox2);
+            flowLayoutPanel1.Dock = DockStyle.Fill;
+            flowLayoutPanel1.Location = new Point(3, 3);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(246, 647);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Location = new Point(3, 3);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(238, 104);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Data Source";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(6, 73);
+            button3.Name = "button3";
+            button3.Size = new Size(226, 23);
+            button3.TabIndex = 2;
+            button3.Text = "Save";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(121, 44);
+            button2.Name = "button2";
+            button2.Size = new Size(111, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Export";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(6, 44);
+            button1.Name = "button1";
+            button1.Size = new Size(111, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Import";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Location = new Point(3, 113);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(238, 164);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "groupBox2";
+            // 
+            // tabPage2
+            // 
+            tabPage2.BackColor = SystemColors.ActiveCaption;
+            tabPage2.Controls.Add(dataGridView1);
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(252, 653);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Stats";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Top;
+            dataGridView1.Location = new Point(3, 3);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(246, 200);
+            dataGridView1.TabIndex = 0;
+            // 
+            // MainAppForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(1064, 681);
+            Controls.Add(splitContainer1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "MainAppForm";
+            Text = "graphSNA";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            flowLayoutPanel1.ResumeLayout(false);
+            groupBox1.ResumeLayout(false);
+            tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private SplitContainer splitContainer1;
+        private Panel panel1;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private TabPage tabPage2;
+        private GroupBox groupBox1;
+        private Button button2;
+        private Button button1;
+        private GroupBox groupBox2;
+        private Button button3;
+        private DataGridView dataGridView1;
+    }
+}
---
+++ src/UI/MainAppForm.cs
@@ -0,0 +1,153 @@
+﻿using graphSNA.Model;
+using System;
+using System.Drawing;
+using System.Drawing.Drawing2D; // SmoothingMode için gerekli
+using System.Windows.Forms;
+
+namespace graphSNA.UI
+{
+    public partial class MainAppForm : Form
+    {
+        // global degiskenler
+        Graph myGraph;
+        Node selectedNode = null;
+
+        // sabitler
+        private const int NodeRadius = 15;
+        private const int NodeSize = NodeRadius*2;
+
+        public MainAppForm()
+        {
+            InitializeComponent();
+
+            // 1. Önce veriyi hazırla (Graph nesnesi burada oluşuyor)
+            InitializeGraphData();
+
+            // 2. Olayları SADECE BURADA bağla (Tekrarı önlemek için)
+            panel1.Paint += GraphCanvas_Paint;
+            panel1.MouseClick += PnlGraph_MouseClick;
+
+            // DoubleBuffered: Titremeyi (flickering) önler, çizimi yumuşatır
+            SetStyle(ControlStyles.DoubleBuffer | ControlStyles.UserPaint | ControlStyles.AllPaintingInWmPaint, true);
+            UpdateStyles();
+        }
+
+        private void InitializeGraphData()
+        {
+            // KRİTİK DÜZELTME: Nesneyi oluşturmayı unutmayın!
+            myGraph = new Graph();
+
+            // Test verileri
+            Node u1 = new Node("Ali", 10, 5, 2);
+            u1.Location = new Point(100, 100);
+
+            Node u2 = new Node("Veli", 20, 10, 5);
+            u2.Location = new Point(300, 200);
+
+            myGraph.AddNode(u1);
+            myGraph.AddNode(u2);
+            myGraph.AddEdge(u1, u2);
+
+            // Buradaki event bağlama kodları silindi (Constructor'a taşındı).
+        }
+
+        private void GraphCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+
+            // Null kontrolü: Eğer graf henüz oluşmadıysa çizim yapma
+            if (myGraph == null) return;
+
+            // 1. KENARLARI ÇİZ
+            using (Pen edgePen = new Pen(Color.Gray, 2))
+            {
+                foreach (Edge edge in myGraph.Edges)
+                {
+                    // Merkezden merkeze çizmek için +NodeRadius ekliyoruz
+                    Point p1 = new Point(edge.Source.Location.X + NodeRadius, edge.Source.Location.Y + NodeRadius);
+                    Point p2 = new Point(edge.Target.Location.X + NodeRadius, edge.Target.Location.Y + NodeRadius);
+
+                    g.DrawLine(edgePen, p1, p2);
+                }
+            }
+
+            // 2. DÜĞÜMLERİ ÇİZ
+            Font font = new Font("Arial", 10, FontStyle.Bold);
+
+            foreach (Node node in myGraph.Nodes)
+            {
+                // Varsayılan Renkler
+                Brush fillBrush = Brushes.LightBlue;
+                Pen borderPen = Pens.Black;
+
+                // SEÇİLİ OLANI VURGULA
+                if (node == selectedNode)
+                {
+                    fillBrush = Brushes.Yellow;
+                    borderPen = new Pen(Color.Red, 3);
+                }
+
+                // Sabitleri kullanarak çizim
+                g.FillEllipse(fillBrush, node.Location.X, node.Location.Y, NodeSize, NodeSize);
+                g.DrawEllipse(borderPen, node.Location.X, node.Location.Y, NodeSize, NodeSize);
+
+                // İsmi yaz
+                g.DrawString(node.Name, font, Brushes.Black, node.Location.X, node.Location.Y - 20);
+            }
+        }
+
+        private void PnlGraph_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (myGraph == null) return;
+
+            Point mousePt = e.Location;
+            bool nodeFound = false;
+
+            foreach (Node node in myGraph.Nodes)
+            {
+                if (IsPointInNode(mousePt, node))
+                {
+                    selectedNode = node;
+                    nodeFound = true;
+                    ShowNodeDetails(node);
+                    break;
+                }
+            }
+
+            // Boşluğa tıklandıysa seçimi kaldır
+            if (!nodeFound)
+            {
+                selectedNode = null;
+                ClearNodeDetails();
+            }
+
+            panel1.Invalidate(); // Yeniden çiz
+        }
+
+        private bool IsPointInNode(Point p, Node n)
+        {
+            // Sabit yarıçapı kullanıyoruz
+            int centerX = n.Location.X + NodeRadius;
+            int centerY = n.Location.Y + NodeRadius;
+
+            // Pisagor
+            double distance = Math.Sqrt(Math.Pow(p.X - centerX, 2) + Math.Pow(p.Y - centerY, 2));
+
+            return distance <= NodeRadius;
+        }
+
+        private void ShowNodeDetails(Node node)
+        {
+            // İleride burayı sağ paneldeki Label'lara bağlayacağız
+            // Şimdilik MessageBox kalabilir
+            string info = $"Ad: {node.Name}\nAktiflik: {node.Activity}\nEtkileşim: {node.Interaction}";
+            MessageBox.Show(info, "Düğüm Bilgisi");
+        }
+
+        private void ClearNodeDetails()
+        {
+            // Sağ paneldeki label'ları temizlemek için burayı kullanacağız
+        }
+    }
+}
