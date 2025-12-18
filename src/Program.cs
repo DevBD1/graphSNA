f46dadab92d2a5e1b379b077b2cc470a3f5e0908
DevBD1 <burakdorman@gmail.com>
2025-12-18 21:45:28 +0300

    feat: Set default culture to "tr-TR".

--- src/Program.cs
+++ src/Program.cs
@@ -10,6 +10,8 @@ namespace graphSNA
         [STAThread]
         static void Main()
         {
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("tr-TR");
+            Application.EnableVisualStyles();
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
