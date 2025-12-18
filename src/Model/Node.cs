4e4dd98a8d7c4388f3fb9157eb36bf5bd4f6b8c1
DevBD1 <burakdorman@gmail.com>
2025-12-18 20:15:23 +0300

    docs: Updated some comment lines

--- src/Model/Node.cs
+++ src/Model/Node.cs
@@ -4,18 +4,17 @@ using System.Drawing; // Required for Point (Location)
 namespace graphSNA.Model
 {
     /// <summary>
-    /// Represents a user (vertex) in the social network graph.
+    ///  Represents a user (vertex) in the social network graph.
     /// </summary>
     public class Node
     {
+        // Unique identifier of each user (e.g., "1", "2", "3", ...)
         public string Id { get; set; }
-        // Unique identifier or name of the user (e.g., "Ali")
+        // Nonunique name of the user (e.g., "Ali")
         public string Name { get; set; }
-
         // Numerical features required for dynamic weight calculation
         public double Activity { get; set; }        // Feature I (Aktiflik)
         public double Interaction { get; set; }     // Feature II (Etkileşim)
-        // Bu artık dışarıdan gelmiyor, biz hesaplayacağız.
         public double ConnectionCount { get; set; } // Feature III (Bağlantı Sayısı)
 
         public System.Drawing.Point Location { get; set; }
@@ -28,14 +27,10 @@ namespace graphSNA.Model
             this.Name = name;
             this.Activity = activity;
             this.Interaction = interaction;
-            // ConnectionCount varsayılan 0 başlar
+            // ConnectionCount == 0 by default, will be set when edges are added
         }
 
         // Override ToString to display the Name easily in ListBoxes or Debugging
-        // public override string ToString() => Name;
-        public override string ToString()
-        {
-            return Name;
-        }
+        public override string ToString() => Name;
     }
 }
