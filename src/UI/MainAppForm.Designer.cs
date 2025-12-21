bb182d9b0e9d0c75539b7765341dd8c087ae6993
Hüseyin Erekmen <huso35goztepe@gmail.com>
2025-12-21 18:21:52 +0300

    feat(ui): integrate Dijkstra and A* algorithms with UI controls
    - Add shortest path calculation event handler
    - Implement algorithm selection via radio buttons
    - Display path cost in text box and detailed result in message box
    - Handle edge cases (empty graph, no path found)
    - Fix naming inconsistencies between Designer and code

--- src/UI/MainAppForm.Designer.cs
+++ src/UI/MainAppForm.Designer.cs
@@ -41,6 +41,12 @@
             groupBox2 = new GroupBox();
             tabPage2 = new TabPage();
             dataGridView1 = new DataGridView();
+            radioDijsktra = new RadioButton();
+            radioAstar = new RadioButton();
+            labelSelectShortestPath = new Label();
+            lblCost = new Label();
+            txtCost = new TextBox();
+            btnFindShortestPath = new Button();
             ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
             splitContainer1.Panel1.SuspendLayout();
             splitContainer1.Panel2.SuspendLayout();
@@ -49,6 +55,7 @@
             tabPage1.SuspendLayout();
             flowLayoutPanel1.SuspendLayout();
             groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
             tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
@@ -57,6 +64,7 @@
             // 
             splitContainer1.Dock = DockStyle.Fill;
             splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Margin = new Padding(6, 7, 6, 7);
             splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -66,16 +74,18 @@
             // splitContainer1.Panel2
             // 
             splitContainer1.Panel2.Controls.Add(tabControl1);
-            splitContainer1.Size = new Size(1064, 681);
-            splitContainer1.SplitterDistance = 800;
+            splitContainer1.Size = new Size(2280, 1680);
+            splitContainer1.SplitterDistance = 1714;
+            splitContainer1.SplitterWidth = 9;
             splitContainer1.TabIndex = 0;
             // 
             // panel1
             // 
             panel1.Dock = DockStyle.Fill;
             panel1.Location = new Point(0, 0);
+            panel1.Margin = new Padding(6, 7, 6, 7);
             panel1.Name = "panel1";
-            panel1.Size = new Size(800, 681);
+            panel1.Size = new Size(1714, 1680);
             panel1.TabIndex = 0;
             // 
             // tabControl1
@@ -84,19 +94,21 @@
             tabControl1.Controls.Add(tabPage2);
             tabControl1.Dock = DockStyle.Fill;
             tabControl1.Location = new Point(0, 0);
+            tabControl1.Margin = new Padding(6, 7, 6, 7);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(260, 681);
+            tabControl1.Size = new Size(557, 1680);
             tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
             tabPage1.BackColor = SystemColors.ActiveCaption;
             tabPage1.Controls.Add(flowLayoutPanel1);
-            tabPage1.Location = new Point(4, 24);
+            tabPage1.Location = new Point(4, 46);
+            tabPage1.Margin = new Padding(6, 7, 6, 7);
             tabPage1.Name = "tabPage1";
-            tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(252, 653);
+            tabPage1.Padding = new Padding(6, 7, 6, 7);
+            tabPage1.Size = new Size(549, 1630);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "Functions";
             // 
@@ -106,9 +118,10 @@
             flowLayoutPanel1.Controls.Add(groupBox1);
             flowLayoutPanel1.Controls.Add(groupBox2);
             flowLayoutPanel1.Dock = DockStyle.Fill;
-            flowLayoutPanel1.Location = new Point(3, 3);
+            flowLayoutPanel1.Location = new Point(6, 7);
+            flowLayoutPanel1.Margin = new Padding(6, 7, 6, 7);
             flowLayoutPanel1.Name = "flowLayoutPanel1";
-            flowLayoutPanel1.Size = new Size(246, 647);
+            flowLayoutPanel1.Size = new Size(537, 1616);
             flowLayoutPanel1.TabIndex = 0;
             // 
             // groupBox1
@@ -116,45 +129,58 @@
             groupBox1.Controls.Add(button3);
             groupBox1.Controls.Add(button2);
             groupBox1.Controls.Add(button1);
-            groupBox1.Location = new Point(3, 3);
+            groupBox1.Location = new Point(6, 7);
+            groupBox1.Margin = new Padding(6, 7, 6, 7);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(238, 104);
+            groupBox1.Padding = new Padding(6, 7, 6, 7);
+            groupBox1.Size = new Size(510, 257);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Text = "Data Source";
             // 
             // button3
             // 
-            button3.Location = new Point(6, 73);
+            button3.Location = new Point(13, 180);
+            button3.Margin = new Padding(6, 7, 6, 7);
             button3.Name = "button3";
-            button3.Size = new Size(226, 23);
+            button3.Size = new Size(484, 57);
             button3.TabIndex = 2;
             button3.Text = "Save";
             button3.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            button2.Location = new Point(121, 44);
+            button2.Location = new Point(259, 109);
+            button2.Margin = new Padding(6, 7, 6, 7);
             button2.Name = "button2";
-            button2.Size = new Size(111, 23);
+            button2.Size = new Size(238, 57);
             button2.TabIndex = 1;
             button2.Text = "Export";
             button2.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            button1.Location = new Point(6, 44);
+            button1.Location = new Point(13, 109);
+            button1.Margin = new Padding(6, 7, 6, 7);
             button1.Name = "button1";
-            button1.Size = new Size(111, 23);
+            button1.Size = new Size(238, 57);
             button1.TabIndex = 0;
             button1.Text = "Import";
             button1.UseVisualStyleBackColor = true;
             // 
             // groupBox2
             // 
-            groupBox2.Location = new Point(3, 113);
+            groupBox2.Controls.Add(btnFindShortestPath);
+            groupBox2.Controls.Add(txtCost);
+            groupBox2.Controls.Add(lblCost);
+            groupBox2.Controls.Add(labelSelectShortestPath);
+            groupBox2.Controls.Add(radioAstar);
+            groupBox2.Controls.Add(radioDijsktra);
+            groupBox2.Location = new Point(6, 278);
+            groupBox2.Margin = new Padding(6, 7, 6, 7);
             groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(238, 164);
+            groupBox2.Padding = new Padding(6, 7, 6, 7);
+            groupBox2.Size = new Size(510, 405);
             groupBox2.TabIndex = 1;
             groupBox2.TabStop = false;
             groupBox2.Text = "groupBox2";
@@ -163,10 +189,11 @@
             // 
             tabPage2.BackColor = SystemColors.ActiveCaption;
             tabPage2.Controls.Add(dataGridView1);
-            tabPage2.Location = new Point(4, 24);
+            tabPage2.Location = new Point(4, 46);
+            tabPage2.Margin = new Padding(6, 7, 6, 7);
             tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(252, 653);
+            tabPage2.Padding = new Padding(6, 7, 6, 7);
+            tabPage2.Size = new Size(549, 1630);
             tabPage2.TabIndex = 1;
             tabPage2.Text = "Stats";
             // 
@@ -174,19 +201,81 @@
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.Dock = DockStyle.Top;
-            dataGridView1.Location = new Point(3, 3);
+            dataGridView1.Location = new Point(6, 7);
+            dataGridView1.Margin = new Padding(6, 7, 6, 7);
             dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(246, 200);
+            dataGridView1.RowHeadersWidth = 76;
+            dataGridView1.Size = new Size(537, 493);
             dataGridView1.TabIndex = 0;
+            // 
+            // radioDijsktra
+            // 
+            radioDijsktra.AutoSize = true;
+            radioDijsktra.Checked = true;
+            radioDijsktra.Location = new Point(24, 102);
+            radioDijsktra.Name = "radioDijsktra";
+            radioDijsktra.Size = new Size(124, 41);
+            radioDijsktra.TabIndex = 0;
+            radioDijsktra.TabStop = true;
+            radioDijsktra.Text = "Dikstra";
+            radioDijsktra.UseVisualStyleBackColor = true;
+            // 
+            // radioAstar
+            // 
+            radioAstar.AutoSize = true;
+            radioAstar.Location = new Point(259, 102);
+            radioAstar.Name = "radioAstar";
+            radioAstar.Size = new Size(70, 41);
+            radioAstar.TabIndex = 1;
+            radioAstar.Text = "A*";
+            radioAstar.UseVisualStyleBackColor = true;
+            // 
+            // labelSelectShortestPath
+            // 
+            labelSelectShortestPath.AutoSize = true;
+            labelSelectShortestPath.Location = new Point(9, 43);
+            labelSelectShortestPath.Name = "labelSelectShortestPath";
+            labelSelectShortestPath.Size = new Size(376, 37);
+            labelSelectShortestPath.TabIndex = 2;
+            labelSelectShortestPath.Text = "Select Shortest Path Algorithm";
+            // 
+            // lblCost
+            // 
+            lblCost.AutoSize = true;
+            lblCost.Location = new Point(24, 161);
+            lblCost.Name = "lblCost";
+            lblCost.Size = new Size(76, 37);
+            lblCost.TabIndex = 3;
+            lblCost.Text = "Cost:";
+            // 
+            // txtCost
+            // 
+            txtCost.Location = new Point(130, 161);
+            txtCost.Name = "txtCost";
+            txtCost.ReadOnly = true;
+            txtCost.Size = new Size(185, 43);
+            txtCost.TabIndex = 4;
+            txtCost.TextChanged += textBox1_TextChanged;
+            // 
+            // btnFindShortestPath
+            // 
+            btnFindShortestPath.Location = new Point(24, 210);
+            btnFindShortestPath.Name = "btnFindShortestPath";
+            btnFindShortestPath.Size = new Size(473, 43);
+            btnFindShortestPath.TabIndex = 5;
+            btnFindShortestPath.Text = "Find Shortest Path";
+            btnFindShortestPath.UseVisualStyleBackColor = true;
+            btnFindShortestPath.Click += findShortestPathButton_Click;
             // 
             // MainAppForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(15F, 37F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaption;
-            ClientSize = new Size(1064, 681);
+            ClientSize = new Size(2280, 1680);
             Controls.Add(splitContainer1);
             Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(6, 7, 6, 7);
             Name = "MainAppForm";
             Text = "graphSNA";
             splitContainer1.Panel1.ResumeLayout(false);
@@ -197,6 +286,8 @@
             tabPage1.ResumeLayout(false);
             flowLayoutPanel1.ResumeLayout(false);
             groupBox1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
             tabPage2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
@@ -216,5 +307,11 @@
         private GroupBox groupBox2;
         private Button button3;
         private DataGridView dataGridView1;
+        private Button btnFindShortestPath;
+        private TextBox txtCost;
+        private Label lblCost;
+        private Label labelSelectShortestPath;
+        private RadioButton radioAstar;
+        private RadioButton radioDijsktra;
     }
 }
--- src/UI/MainAppForm.cs
+++ src/UI/MainAppForm.cs
@@ -1,98 +1,75 @@
 ﻿using graphSNA.Model;
 using System;
 using System.Drawing;
-using System.Drawing.Drawing2D; // SmoothingMode için gerekli
+using System.Drawing.Drawing2D;
 using System.Windows.Forms;
+using System.Linq;
 
 namespace graphSNA.UI
 {
     public partial class MainAppForm : Form
     {
-        // global degiskenler
         Graph myGraph;
         Node selectedNode = null;
 
-        // sabitler
         private const int NodeRadius = 15;
-        private const int NodeSize = NodeRadius*2;
+        private const int NodeSize = NodeRadius * 2;
 
         public MainAppForm()
         {
             InitializeComponent();
-
-            // 1. Önce veriyi hazırla (Graph nesnesi burada oluşuyor)
             InitializeGraphData();
 
-            // 2. Olayları SADECE BURADA bağla (Tekrarı önlemek için)
+            // Bind events manually to ensure single subscription
             panel1.Paint += GraphCanvas_Paint;
             panel1.MouseClick += PnlGraph_MouseClick;
 
-            // DoubleBuffered: Titremeyi (flickering) önler, çizimi yumuşatır
+            // Enable DoubleBuffering to prevent rendering flicker
             SetStyle(ControlStyles.DoubleBuffer | ControlStyles.UserPaint | ControlStyles.AllPaintingInWmPaint, true);
             UpdateStyles();
         }
 
         private void InitializeGraphData()
         {
-            // KRİTİK DÜZELTME: Nesneyi oluşturmayı unutmayın!
             myGraph = new Graph();
 
-            // Test verileri
-            Node u1 = new Node("Ali", 10, 5, 2);
-            u1.Location = new Point(100, 100);
-
-            Node u2 = new Node("Veli", 20, 10, 5);
-            u2.Location = new Point(300, 200);
+            // Initialize with dummy data for testing
+            Node u1 = new Node("Ali", 10, 5, 2) { Location = new Point(100, 100) };
+            Node u2 = new Node("Veli", 20, 10, 5) { Location = new Point(300, 200) };
 
             myGraph.AddNode(u1);
             myGraph.AddNode(u2);
             myGraph.AddEdge(u1, u2);
-
-            // Buradaki event bağlama kodları silindi (Constructor'a taşındı).
         }
 
         private void GraphCanvas_Paint(object sender, PaintEventArgs e)
         {
+            if (myGraph == null) return;
+
             Graphics g = e.Graphics;
             g.SmoothingMode = SmoothingMode.AntiAlias;
 
-            // Null kontrolü: Eğer graf henüz oluşmadıysa çizim yapma
-            if (myGraph == null) return;
-
-            // 1. KENARLARI ÇİZ
+            // Draw Edges
             using (Pen edgePen = new Pen(Color.Gray, 2))
             {
                 foreach (Edge edge in myGraph.Edges)
                 {
-                    // Merkezden merkeze çizmek için +NodeRadius ekliyoruz
                     Point p1 = new Point(edge.Source.Location.X + NodeRadius, edge.Source.Location.Y + NodeRadius);
                     Point p2 = new Point(edge.Target.Location.X + NodeRadius, edge.Target.Location.Y + NodeRadius);
-
                     g.DrawLine(edgePen, p1, p2);
                 }
             }
 
-            // 2. DÜĞÜMLERİ ÇİZ
+            // Draw Nodes
             Font font = new Font("Arial", 10, FontStyle.Bold);
 
             foreach (Node node in myGraph.Nodes)
             {
-                // Varsayılan Renkler
-                Brush fillBrush = Brushes.LightBlue;
-                Pen borderPen = Pens.Black;
+                Brush fillBrush = (node == selectedNode) ? Brushes.Yellow : Brushes.LightBlue;
+                Pen borderPen = (node == selectedNode) ? new Pen(Color.Red, 3) : Pens.Black;
 
-                // SEÇİLİ OLANI VURGULA
-                if (node == selectedNode)
-                {
-                    fillBrush = Brushes.Yellow;
-                    borderPen = new Pen(Color.Red, 3);
-                }
-
-                // Sabitleri kullanarak çizim
                 g.FillEllipse(fillBrush, node.Location.X, node.Location.Y, NodeSize, NodeSize);
                 g.DrawEllipse(borderPen, node.Location.X, node.Location.Y, NodeSize, NodeSize);
-
-                // İsmi yaz
                 g.DrawString(node.Name, font, Brushes.Black, node.Location.X, node.Location.Y - 20);
             }
         }
@@ -101,12 +78,11 @@ namespace graphSNA.UI
         {
             if (myGraph == null) return;
 
-            Point mousePt = e.Location;
             bool nodeFound = false;
 
             foreach (Node node in myGraph.Nodes)
             {
-                if (IsPointInNode(mousePt, node))
+                if (IsPointInNode(e.Location, node))
                 {
                     selectedNode = node;
                     nodeFound = true;
@@ -115,39 +91,77 @@ namespace graphSNA.UI
                 }
             }
 
-            // Boşluğa tıklandıysa seçimi kaldır
             if (!nodeFound)
             {
                 selectedNode = null;
                 ClearNodeDetails();
             }
 
-            panel1.Invalidate(); // Yeniden çiz
+            panel1.Invalidate(); // Trigger redraw to update selection state
         }
 
         private bool IsPointInNode(Point p, Node n)
         {
-            // Sabit yarıçapı kullanıyoruz
             int centerX = n.Location.X + NodeRadius;
             int centerY = n.Location.Y + NodeRadius;
 
-            // Pisagor
+            // Euclidean distance check
             double distance = Math.Sqrt(Math.Pow(p.X - centerX, 2) + Math.Pow(p.Y - centerY, 2));
-
             return distance <= NodeRadius;
         }
 
         private void ShowNodeDetails(Node node)
         {
-            // İleride burayı sağ paneldeki Label'lara bağlayacağız
-            // Şimdilik MessageBox kalabilir
-            string info = $"Ad: {node.Name}\nAktiflik: {node.Activity}\nEtkileşim: {node.Interaction}";
-            MessageBox.Show(info, "Düğüm Bilgisi");
+            string info = $"Name: {node.Name}\nActivity: {node.Activity}\nInteraction: {node.Interaction}";
+            MessageBox.Show(info, "Node Info");
         }
 
         private void ClearNodeDetails()
         {
-            // Sağ paneldeki label'ları temizlemek için burayı kullanacağız
+            // Placeholder for UI cleanup logic
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e) { }
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
+
+        private void findShortestPathButton_Click(object sender, EventArgs e)
+        {
+            if (myGraph == null || myGraph.Nodes.Count < 2)
+            {
+                MessageBox.Show("Graph is not ready.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var start = myGraph.Nodes.First();
+            var goal = myGraph.Nodes.Last();
+
+            IShortestPathAlgorithm algorithm;
+
+            // Note: 'radioDijsktra' contains a typo in Designer, kept for consistency
+            if (radioDijsktra.Checked)
+                algorithm = new DijkstraAlgorithm();
+            else if (radioAstar.Checked)
+                algorithm = new AStarAlgorithm();
+            else
+            {
+                MessageBox.Show("Please select an algorithm.", "Warning");
+                return;
+            }
+
+            var manager = new ShortestPathManager(algorithm);
+            var (path, cost) = manager.Calculate(myGraph, start, goal);
+
+            if (path.Count == 0)
+            {
+                txtCost.Text = "No Path";
+                MessageBox.Show("No path found.", "Result");
+            }
+            else
+            {
+                string pathStr = string.Join(" → ", path.Select(n => n.Name));
+                txtCost.Text = $"{cost:F2}";
+                MessageBox.Show($"Path: {pathStr}\nCost: {cost:F2}", "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
     }
 }
