d054b3b645fb1155f2528c58bc53e2116431ef6c
DevBD1 <burakdorman@gmail.com>
2026-01-02 14:34:18 +0300

    fix: Fixed InputNodeForm's UI scale problems.

--- src/UI/InputNodeForm.Designer.cs
+++ src/UI/InputNodeForm.Designer.cs
@@ -2,36 +2,128 @@
 {
     partial class InputNodeForm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        // Kontrol elemanları (Artık burada tanımlı)
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtActivity;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtInteraction;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+
         protected override void Dispose(bool disposing)
         {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
+            if (disposing && (components != null)) components.Dispose();
             base.Dispose(disposing);
         }
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtActivity = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtInteraction = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İsim:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(140, 22);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(180, 23);
+            this.txtName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Activity (0-1):";
+            // 
+            // txtActivity
+            // 
+            this.txtActivity.Location = new System.Drawing.Point(140, 62);
+            this.txtActivity.Name = "txtActivity";
+            this.txtActivity.Size = new System.Drawing.Size(180, 23);
+            this.txtActivity.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Interaction (0-1):";
+            // 
+            // txtInteraction
+            // 
+            this.txtInteraction.Location = new System.Drawing.Point(140, 102);
+            this.txtInteraction.Name = "txtInteraction";
+            this.txtInteraction.Size = new System.Drawing.Size(180, 23);
+            this.txtInteraction.TabIndex = 5;
+            // 
+            // btnOk
+            // 
+            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOk.Location = new System.Drawing.Point(50, 160);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(100, 30);
+            this.btnOk.TabIndex = 6;
+            this.btnOk.Text = "Kaydet";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(180, 160);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(100, 30);
+            this.btnCancel.TabIndex = 7;
+            this.btnCancel.Text = "İptal";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // InputNodeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(350, 211);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.txtInteraction);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtActivity);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "InputNodeForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "InputNodeForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
--- src/UI/InputNodeForm.cs
+++ src/UI/InputNodeForm.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace graphSNA.UI
 {
@@ -11,62 +12,29 @@ namespace graphSNA.UI
     public partial class InputNodeForm : Form
     {
         // Properties bound to text fields - accessed after dialog closes
-        public string NodeName { get { return txtName.Text; } set { txtName.Text = value; } }
-        public float Activity { get { float.TryParse(txtActivity.Text, out float v); return v; } set { txtActivity.Text = value.ToString(); } }
-        public float Interaction { get { float.TryParse(txtInteraction.Text, out float v); return v; } set { txtInteraction.Text = value.ToString(); } }
+        public string NodeName { get; set; }
+        public float Activity { get; set; }
+        public float Interaction { get; set; }
 
-        private TextBox txtName, txtActivity, txtInteraction;
-        private Button btnSave, btnCancel;
-
-        public InputNodeForm(string title = "Düğüm Bilgileri")
+        // Constructor
+        public InputNodeForm(string title)
         {
+            // Tüm sihri bu metod yapacak (Designer dosyasındaki kodlar)
+            InitializeComponent();
+
             this.Text = title;
-            this.Size = new Size(300, 220);
-            this.StartPosition = FormStartPosition.CenterParent;
-            this.FormBorderStyle = FormBorderStyle.FixedDialog;
-            this.MaximizeBox = false;
-
-            int y = 20;
-            int lblX = 20;
-            int txtX = 130;
-
-            // Name field
-            AddLabel("İsim:", lblX, y);
-            txtName = AddTextBox(txtX, y);
-
-            // Activity field (0-100)
-            y += 40;
-            AddLabel("Aktiflik (0-100):", lblX, y);
-            txtActivity = AddTextBox(txtX, y);
-
-            // Interaction field (0-100)
-            y += 40;
-            AddLabel("Etkileşim (0-100):", lblX, y);
-            txtInteraction = AddTextBox(txtX, y);
-
-            // Buttons
-            y += 50;
-            btnSave = new Button() { Text = "Kaydet", DialogResult = DialogResult.OK, Location = new Point(50, y), Size = new Size(80, 30), BackColor = Color.LightGreen };
-            btnCancel = new Button() { Text = "İptal", DialogResult = DialogResult.Cancel, Location = new Point(150, y), Size = new Size(80, 30) };
-
-            this.Controls.Add(btnSave);
-            this.Controls.Add(btnCancel);
-            this.AcceptButton = btnSave;
         }
 
-        // Helper: creates and adds a label to the form
-        private void AddLabel(string text, int x, int y)
+        // Kaydet butonuna basılınca çalışacak olay (Designer'da bağladık)
+        private void btnOk_Click(object sender, EventArgs e)
         {
-            Label l = new Label() { Text = text, Location = new Point(x, y + 3), AutoSize = true };
-            this.Controls.Add(l);
-        }
+            NodeName = txtName.Text;
 
-        // Helper: creates and adds a textbox to the form
-        private TextBox AddTextBox(int x, int y)
-        {
-            TextBox t = new TextBox() { Location = new Point(x, y), Width = 140 };
-            this.Controls.Add(t);
-            return t;
+            float.TryParse(txtActivity.Text, out float act);
+            float.TryParse(txtInteraction.Text, out float inter);
+
+            Activity = act;
+            Interaction = inter;
         }
     }
 }
